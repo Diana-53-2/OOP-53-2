37050766a31f04ca6a6822d4a9c5c1a3ddf591cd
Diana-53-2 <adylova016@gmail.com>
2025-04-24 18:24:27 +0600

    hw5 and requirements

--- Homeworks/hw5.py
+++ Homeworks/hw5.py
@@ -1,3 +1,12 @@
-import pandas as pd
 
-data = {}
+#Импорт colorama
+from colorama import init, Fore, Back, Style
+
+# Инициализация colorama
+init()
+
+print(Fore.RED + 'текст красного цвета')  #Fore для получения цветного шрифта
+print(Fore.GREEN + 'А этот зелёного')
+print(Back.YELLOW + Fore.BLACK + 'Чёрный текст на жёлтом фоне')  #Back для цветного фона
+print(Style.BRIGHT + 'Яркий текст')       #Style для того чтоб сделать жирным текст
+print(Style.RESET_ALL + 'Сброс стилей и краски') # Style.RESET_ALL для сброса
